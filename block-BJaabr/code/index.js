1753c84b5b19f3ac42a62525c2ab0c8e2aa1bc8f
Devanand kumar <deva.zebu288@gmail.com>
2022-11-27 16:08:43 +0530

    Assignments: Function Type

--- block-BJaabr/code/index.js
+++ block-BJaabr/code/index.js
@@ -1,7 +1,8 @@
 // -ook at the example below and do the same for other problems.
 
 /*
-Write a function named convertToString that accepts a number and converts the number into a string (hint: use String()) and return the converted value.
+Write a function named convertToString that accepts a number and converts
+the number into a string (hint: use String()) and return the converted value.
 
 Do the following for the given problem:
 */
@@ -9,7 +10,8 @@ Do the following for the given problem:
 // - Write a Function Decleration
 function convertToString(n) {
   return String(n);
-}
+};
+
 
 // - Write a Function Expression
 let convertToString = function (n) {
@@ -42,38 +44,77 @@ Do the following with above problem.
 
 // - Write a Function Declaration
 
+function addOne(n) {
+  return n + 1;
+}
+
 // - Write a Function Expression
+
+let addOne = function (n) {
+  return n + 1;
+}
 
 // - Write an Arrow Function without curly brackets(if possible)
 
+let addOne = (n) => n + 1;
+
 // - Write an Arrow Function with curly brackets
+
+let addOne = (n) => {
+  return n + 1;
+};
 
 // - Execute the function
 
+addOne();
+
 // - Execute the function and store the return value in a variable.
+ 
+let num = addOne();
 
 // - What is the typeof returnValue
+
+"number"
 
 /* 2. Subtract one
 
 Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
 
-Do the following with above problem.
+Do the  following with above problem.
 */
 
 // - Write a Function Declaration
 
+function substractOne(n) {
+  return n - 1;
+}
+
 // - Write a Function Expression
+
+let substractOne = function (n) {
+  return n - 1;
+}
 
 // - Write an Arrow Function without curly brackets(if possible)
 
+let substractOne = (n) => n - 1;
+
 // - Write an Arrow Function with curly brackets
+
+let substractOne = (n) {
+  return n - 1;
+}
 
 // - Execute the function
 
+substractOne();
+
 // - Execute the function and store the return value in a variable.
 
+substractOne(`n - 1`);
+
 // - What is the typeof returnValue
+"number"
 
 /* 3. Add two numbers
 
@@ -150,11 +191,44 @@ Do the following with above problem.
 
 // - Write a Function Declaration
 
+function oddOrEven (num) {
+  if (num % 2 === 0) {
+    return `Number is Even`;
+  } else {
+    return `Number is Odd`;
+  }
+}
+
 // - Write an anonymous Function Expression
+
+let oddOrEven = function (num) {
+  if (num % 2 === 0) {
+    return `Number is Even`;
+  } else {
+    return `Number is Odd`;
+  }
+}
+
 
 // - Write an named Function Expression
 
+let oddOrEven = function (num) {
+  if (num % 2 === 0) {
+    return `Number is Even`;
+  } else {
+    return `Number is Odd`;
+  }
+}
+
 // - Write an Arrow Function without curly brackets (hint: use ternary operator)
+
+let oddOrEven = (num) => {
+  if (num % 2 === 0) {
+    return `Number is Even`;
+  } else {
+    return `Number is Odd`;
+  }
+}
 
 // - Write an Arrow Function with curly brackets
 
